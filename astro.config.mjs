573fc3887ddb19c1941d4e37ac4e667f60da25d5
misco <miscojohnes1@gmail.com>
2025-05-11 00:59:38 +0200

    improved website

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,4 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'http://uptoinfinityincremental.work.gd',
-  publicDir: './public', // Asegúrate de que esta configuración sea correcta
 });
